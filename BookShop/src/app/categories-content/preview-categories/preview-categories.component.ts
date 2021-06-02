0809f0e45340fadc24254a8c62b7329be3fa3d44
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-02 15:25:54 +0200

    Added grid to preview categories component

--- BookShop/src/app/categories-content/preview-categories/preview-categories.component.ts
+++ BookShop/src/app/categories-content/preview-categories/preview-categories.component.ts
@@ -12,7 +12,10 @@ export class PreviewCategoriesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  categories: [];
+  categories=[
+    {id:1, name:'first'},
+    {id:2, name:'second'}
+  ];
 
   // Need to add api connection for editing and deleting category
 
