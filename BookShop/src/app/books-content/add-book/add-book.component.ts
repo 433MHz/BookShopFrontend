b93519411f70b97402b2f81d48c1652c2d396183
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-05 17:05:30 +0200

    Added api for adding books (Category is not saving properly)

--- BookShop/src/app/books-content/add-book/add-book.component.ts
+++ BookShop/src/app/books-content/add-book/add-book.component.ts
@@ -1,4 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export class Category{
+  constructor(
+    id: number,
+    name: string
+  ){}
+}
+
+export class Book{
+  constructor(
+    id: number,
+    title: string,
+    authorFirstName: string,
+    authorSurname: string,
+    year: number,
+    category: Category
+  ){}
+}
 
 @Component({
   selector: 'app-add-book',
@@ -7,30 +26,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllCategories();
   }
 
   // Need to fetch it from api in future
-  options = [
-    {id: 1, name: 'horror'},
-    {id: 2, name: 'komedia'},
-    {id: 3, name: 'test'},
-  ];
 
   bookData = {
-    bookTitle: '',
+    id: null,
+    title: '',
     authorFirstName: '',
-    authorLastName: '',
-    yearOfPublishment: null,
-    category: '1'
+    authorSurname: '',
+    year: 1999,
+    category: null
   }
 
+  categories: Category
+
+  selectedOption: string;
+
+  responseMessage: string;
 
   // Need to add api in future
   sendBookToBackend(){
-    console.log(this.bookData);
+
+    this.httpClient.post<Category>('http://localhost:8081/categories/getById', this.selectedOption, {headers:new HttpHeaders().set('Content-Type', 'application/json'), responseType: 'json'}).subscribe(
+      data => {
+        this.bookData.category = data;
+      }
+    )
+
+    console.log(this.bookData)
+
+    this.httpClient.post('http://localhost:8081/books/add', this.bookData, {headers:new HttpHeaders().set('Content-Type', 'application/json'), responseType: 'text'}).subscribe(
+      data => {
+        this.responseMessage = data
+      }
+    )
+
+  }
+
+  getAllCategories(){
+
+    this.httpClient.get<any>('http://localhost:8081/categories/getAll').subscribe(
+      response => {
+      this.categories = response
+      }
+    )
   }
 
 }
