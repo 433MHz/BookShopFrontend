50142c7364353dfa711c6c352c75c767e721f5ce
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-05 13:18:37 +0200

    api added to categories preview component

--- BookShop/src/app/categories-content/preview-categories/preview-categories.component.ts
+++ BookShop/src/app/categories-content/preview-categories/preview-categories.component.ts
@@ -1,4 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export class Categories{
+  constructor(
+    id: number,
+    name: string
+  ){}
+}
+
 
 @Component({
   selector: 'app-preview-categories',
@@ -7,17 +16,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewCategoriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
-  categories=[
-    {id:1, name:'first'},
-    {id:2, name:'second'}
-  ];
+  categories: Categories;
 
   // Need to add api connection for editing and deleting category
+
+  getCategories(){
+    this.httpClient.get<any>('http://localhost:8081/categories/getAll').subscribe(
+      response => {
+        this.categories = response;
+      }
+    )
+  }
 
   editCategoryName(categoryID:number){
 
