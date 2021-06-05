bff4c16d0abe2a8d535f19ffefaf1d4f19332bee
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-06-05 15:03:44 +0200

    add category component now have working api connection

--- BookShop/src/app/categories-content/add-category/add-category.component.ts
+++ BookShop/src/app/categories-content/add-category/add-category.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export class Category {
+  constructor(
+  id: number,
+  name: string
+  ){}
+}
 
 @Component({
   selector: 'app-add-category',
@@ -7,13 +15,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient)
+     {}
 
   ngOnInit(): void {
   }
 
   categoryName: string;
+  response: string;
 
+  category = {
+    name: null
+  }
 
   // Need to add function to check if category name exist in database
+
+  postCategory(){
+    this.category.name = this.categoryName;
+
+    this.httpClient.post('http://localhost:8081/categories/add' , this.category, {headers:new HttpHeaders().set('Content-Type', 'application/json'), responseType: 'text'}).subscribe(
+      data => {this.response = data;
+      }
+    )
+  
+  }
 }
